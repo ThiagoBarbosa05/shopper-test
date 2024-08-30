e721f1afe4f0007b264ca65e455a6c76d5290061
thiago <thiagon16@hotmail.com>
2024-08-30 17:39:03 -0300

    fix(upload-image-use-case): filepath

--- src/use-cases/upload-image-use-case.ts
+++ src/use-cases/upload-image-use-case.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import {fileURLToPath} from 'url'
 import { Uploader } from '../core/storage/uploader'
 
 type UploadImageUseCaseInput = {
@@ -19,6 +20,11 @@ export class UploadImageUseCase {
   async execute({ imageBase64 }: UploadImageUseCaseInput): Promise<UploadImageUseCaseOutput> {
 
     const buffer = Buffer.from(imageBase64, 'base64')
+
+
+    const __filename = fileURLToPath(import.meta.url)
+ 
+    const __dirname = path.dirname(__filename)
 
     const filepath = path.join(__dirname, "tempImage.png")
 
